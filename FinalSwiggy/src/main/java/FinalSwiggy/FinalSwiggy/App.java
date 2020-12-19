9ad47d8f64fd8ad9ae4ca9d4c72b7d36e118ae41
msa4012 <madhuri.adapala@gmail.com>
2020-12-19 22:23:45 +0000

    FinalSwiggy first commit

---
+++ FinalSwiggy/src/main/java/FinalSwiggy/FinalSwiggy/App.java
@@ -0,0 +1,13 @@
+package FinalSwiggy.FinalSwiggy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
